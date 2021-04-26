6d2d969fef2a3fb7d1d49da3c7c102ced5687b64
chessyu <472734301@qq.com>
2021-04-27 00:53:35 +0800

    commit
    ....

---
+++ src/components/list.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="product-list" >
+        <h6>{{title}}</h6>
+        <p v-for="(item,index) in data" :key="index">{{item.content}}</p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type:String,
+            require:true
+        },
+        data:{
+            type:Array,
+            require:true
+        }
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+
+<style>
+    .product-list{
+        padding: 20px 0;
+        color: var(--fontColor);
+        text-align: left;
+    }
+    .product-list h6{
+        font-size: 13px;
+        font-weight: bold;
+        padding-bottom: 10px;
+        color: black;
+    }
+    .product-list p{
+        line-height: 2;
+        font-size: 12px;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="contiter">
+    <div class="header">
+      <div class="company_logo">
+        <img   src="../assets/logo.png" alt="">
+      </div>
+      <span class="info-name">陈壮壮方案</span>
+      <span class="code"> 二维码 </span>
+    </div>
+    <div class="info">
+      <div class="info-cont">
+        <p>姓名：陈壮壮</p>
+        <p>编号：ABCKDEKLJ8K9237UEN</p>
+      </div>
+    </div>
+    <div class="cace-report">
+      <list :title="item.title" :data="item.data" v-for="(item,i) in data" :key="i" />
+    </div>
+  </div>
+</template>
+
+<script>
+import config from '../config'
+import List from '../components/list.vue'
+
+export default {
+  components:{
+    List
+  },
+  data(){
+    return{
+      data :[
+        {
+          title:'肌肤症状:',
+          data:[
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            }
+          ]
+        },
+        {
+          title:'问题诊断:',
+          data:[
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            }
+          ]
+        },
+        {
+          title:'解决方案:',
+          data:[
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            },
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            },
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            },
+          ]
+        },
+        {
+          title:'温馨提示:',
+          data:[
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            },
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            },
+            {
+              content:'aaaajalksdfjlas;kfjas;lfkjas;dfkkjkj;l'
+            }
+          ]
+        },
+      ]
+    }
+  }
+}
+</script>
